d8284642b0b9c87f52bbf3ef55a04791fb35db31
matthews <cnusrinivasu@gmail.com>
2025-09-17 19:11:36 +0530

    modified code backend3

--- backend.sh
+++ backend.sh
@@ -62,7 +62,7 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? "Downloading backend application code"
 
 cd /app
-#rm -rf /app/* # remove the existing code
+rm -rf /app/* # remove the existing code
 unzip /tmp/backend.zip &>>$LOG_FILE
 #VALIDATE $? "Extracting backend application code"
 
@@ -74,7 +74,7 @@ cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.serv
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h mysql.learnaws.space -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+mysql -h mysql.learnaws.space -uroot -p ExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
 VALIDATE $? "Schema loading"
 
 systemctl daemon-reload &>>$LOG_FILE
