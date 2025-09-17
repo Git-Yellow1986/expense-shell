9832e4fd70227e6f1fd222e203098239c1700d07
matthews <cnusrinivasu@gmail.com>
2025-09-17 14:56:54 +0530

    extracting frontend code

--- frontend.sh
+++ frontend.sh
@@ -44,7 +44,7 @@ rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIDATE $? "Removed exists html code"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
-VALIDATE $? "Downloading frontend application code"
+#VALIDATE $? "Downloading frontend application code"
 
 
 cd /usr/share/nginx/html
