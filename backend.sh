cba933b4f279b5b034a8e1dd605c206a63ffe8fe
matthews <cnusrinivasu@gmail.com>
2025-09-17 17:54:50 +0530

    modified code backend

--- backend.sh
+++ backend.sh
@@ -114,7 +114,7 @@ VALIDATE $? "Downloading backend application code"
 cd /app
 rm -rf /app/* # remove the existing code
 unzip /tmp/backend.zip &>>$LOG_FILE
-#VALIDATE $? "Extracting backend application code"
+VALIDATE $? "Extracting backend application code"
 
 npm install &>>$LOG_FILE
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
@@ -124,7 +124,7 @@ cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.serv
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h 172.31.30.124 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+mysql -h mysql.learnaws.space -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
 VALIDATE $? "Schema loading"
 
 systemctl daemon-reload &>>$LOG_FILE
